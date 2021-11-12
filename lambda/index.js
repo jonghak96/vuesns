042628c571a419443d3a3f134347cec75fcb6e8e
jonghak96 <67566238+jonghak96@users.noreply.github.com>
2021-11-12 15:13:47 +0900

    배포중 람다

---
+++ lambda/index.js
@@ -0,0 +1,32 @@
+const AWS = require("aws-sdk");
+const Sharp = require("sharp");
+
+const S3 = new AWS.S3({region: "us-east-2"});
+
+exports.handler = async (event, context, callback) => {
+
+    const Bucket = event.Records[0].s3.bucket.name;
+    const Key = event.Records[0].s3.object.key;
+    const filename = Key.split('/')[Key.split('/').length - 1];
+    const ext = Key.split('.')[Key.split('.').length - 1];
+    const requiredFormat = ext === "jpg" ? "jpeg" : ext;
+
+    try {
+        const s3Object = await S3.getObject({
+            Bucket,
+            Key,
+        }).promise();
+        const resizedImage = await Sharp(s3Object.body).resize(800, 800, { fit: "inside" }).toFormat(requiredFormat).toBuffer();
+        await S3.putObject({
+            Body: resizedImage,
+            Bucket,
+            Key: `thumbnail/${filename}`,
+        }).promise();
+        return callback(null, `thumbnail/${filename}`);
+        
+    } catch (e) {
+        console.error(e);
+        return callback(e);
+    }
+
+}
